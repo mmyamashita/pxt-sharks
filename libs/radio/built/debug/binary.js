7b67aee5ddb0bbe8d645d8920cb0031d2f2d0082
mmyamashita <myamashita@thetech.org>
2019-02-05 17:14:38 -0800

    rebuilt package

--- libs/radio/built/debug/binary.js
+++ libs/radio/built/debug/binary.js
@@ -12,7 +12,7 @@ if (yieldSteps-- < 0 && maybeYield(s, step, r0)) return null;
 switch (step) {
   case 0:
 
-    r0 = (1 * 25.4);
+    r0 = (1.25 * 25.4);
     s.tmp_0 = r0;
     r0 = globals.diam___339;
     r0 = s.tmp_0;
--- projects/Untitled-2/main.ts
+++ projects/Untitled-2/main.ts
@@ -1,1 +1,3 @@
-
+let strip: basic.Strip = null
+strip = basic.create(NeoPixelColors.Blue)
+forward.forward1(turn.totalDegrees())
